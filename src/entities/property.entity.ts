5e1144744dc52893c730b23110400be2692f2cf4
AnhLDP <leducphananh@gmail.com>
2025-05-26 14:41:14 +0700

    feat(property): add PropertyFeature entity and establish one-to-one relationship with Property

--- src/entities/property.entity.ts
+++ src/entities/property.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { PropertyFeature } from './propertyFeature.entity';
 
 @Entity()
 export class Property {
@@ -13,4 +14,11 @@ export class Property {
 
   @Column({ default: 0 })
   price: number;
+
+  @OneToOne(
+    () => PropertyFeature,
+    (propertyFeature) => propertyFeature.property,
+    { cascade: true },
+  )
+  propertyFeature: PropertyFeature;
 }
